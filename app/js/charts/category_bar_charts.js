041e08dc090f1a01c5952ea83b353bed84e5cbf6
RAYA DESAWADE <rayalynn@gmail.com>
2013-03-20 12:25:09 -0700

    Try to refactor code and made it worse.

--- app/js/charts/category_bar_charts.js
+++ app/js/charts/category_bar_charts.js
@@ -2,58 +2,6 @@
  *
  * */
 
-var charts = {};
-charts.settings = {
-  $container: $('div.data'),
-  NEGATIVE_COLOR:  '#D7772A',
-  WARNING_COLOR:  '#E9AA35',
-  POSITIVE_COLOR:  '#206CA3'
-};
-
-//Default options hash used by HighCharts
-charts.barOptions = {
-  credits: {
-    enabled: false
-  },
-  chart: {
-    type: 'bar',
-    backgroundColor: 'transparent'
-  },
-  title: {
-    text: ''
-  },
-  plotOptions: {
-    series: {
-      borderWidth: 0,
-      shadow: false
-    }
-  },
-  xAxis: {
-    labels: {
-      enabled: false
-    },
-    tickLength: 0
-  },
-  yAxis: {
-    gridLineWidth: 0,
-    tickLength: 0,
-    labels: {
-      enabled: false
-    },
-    title: {
-      text: ''
-    }
-  },
-  legend: {
-    enabled: false
-  },
-  series: [{}]
-};
-
-var createChart = function(options) {
-  var chart = new Highcharts.Chart(options);
-};
-
 var getAverage = function(data) {
 
   var average = 0;
@@ -90,31 +38,34 @@ var setDataObj = function(data) {
 
 var drawColumn = function(data, colNumber) {
 
-  charts.barOptions.chart.renderTo = charts.settings.$container[colNumber];
-  charts.barOptions.series = [];
+  var graphOptions = charts.barOptions();
+  graphOptions.chart.renderTo = charts.settings.$container[colNumber];
+  graphOptions.series = [];
   var seriesObj = {};
   seriesObj["data"] = setDataObj(data);
 
   var average = getAverage(data);
-  charts.barOptions.yAxis.plotLines = [{
+  graphOptions.yAxis.plotLines = [{
     color: 'gray',
       width: 1,
       value: average,
       dashStyle: 'longdashdot'
   }];
-  charts.barOptions.series.push(seriesObj);
-  createChart(charts.barOptions);
+  graphOptions.series.push(seriesObj);
+  charts.createChart(graphOptions);
 };
 
+//Convert the data into JSON objects and store it in an array
 var loadDataFile = function(string) {
+
   var brands = JSON.parse(string);
+
   var salesData = [];
   var marginData = [];
   var volumeData = [];
   var profitData = [];
   var transactionsData = [];
   var impactData = [];
-  var count = 0;
 
   //Load up data in proper arrays
   for (var item in brands) {
@@ -138,7 +89,7 @@ var loadDataFile = function(string) {
   drawColumn(impactData, 5);
 };
 
-
+//Load the Incremental Category file from the server
 $.ajax({
   url: '../../data/mod_category_data_inc.js',
   success: function(data) {
