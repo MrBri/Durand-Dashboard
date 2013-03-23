70df22296117ef893be47a617194c50c017382b2
michael <michaelglenadams@gmail.com>
2013-03-23 00:40:17 -0700

    ng-repeat for category names

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,10 +12,14 @@ function loginCtrl($scope) {
 }
 loginCtrl.$inject = ['$scope'];
 
-function homeCtrl ($scope) {
+function homeCtrl ($scope, $http) {
   $scope.catDataInc = glb.categoryDataIncremental;
+  $http.get('/app/data/mod_category_data_inc.js').success(function(data){
+    $scope.categories = data;
+    console.log($scope.categories);
+  });
 }
-homeCtrl.$inject = ['$scope'];
+homeCtrl.$inject = ['$scope', '$http'];
 
 function twoBytwoCtrl($scope, $http) {
   $http.get('data/twoBytwoData.js').success(function(data){
