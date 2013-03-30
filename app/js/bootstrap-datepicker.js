85d08a593d285088a1acae7057b819ba7453c46e
Tony Thomson <thomson@me.com>
2013-03-30 11:34:14 -0700

    Fixes z-index bug with date-pickers and arrow-positioning bug in totals of Brands page.

--- app/js/bootstrap-datepicker.js
+++ app/js/bootstrap-datepicker.js
@@ -353,7 +353,7 @@
 						if(this.isInline) return;
 			var zIndex = parseInt(this.element.parents().filter(function() {
 							return $(this).css('z-index') != 'auto';
-						}).first().css('z-index'))+10;
+						}).first().css('z-index'))+1000;
 			var offset = this.component ? this.component.parent().offset() : this.element.offset();
 			var height = this.component ? this.component.outerHeight(true) : this.element.outerHeight(true);
 			this.picker.css({
